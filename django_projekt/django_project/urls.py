a5929e852b504934ae675991a03d058442c627c3
Koey124 <pietrek.q@wp.pl>
2021-01-06 15:42:49 +0100

    Before bug with ListView

--- django_projekt/django_project/urls.py
+++ django_projekt/django_project/urls.py
@@ -1,7 +1,6 @@
-"""django_projekt URL Configuration
-
+"""django_project URL Configuration
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -27,8 +26,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
-
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
